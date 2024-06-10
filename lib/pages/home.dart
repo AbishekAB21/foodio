599afd363f93f090b0c562a44b131d2b3882fefc
AbishekAB21 <abishekab1805@outlook.com>
2024-06-10 09:46:04 +0530

    commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -18,7 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: appcolor.backgroundColor,
       body: Container(
-        margin: EdgeInsets.only(top: 50, left: 20, right: 20),
+        margin: EdgeInsets.only(top: 50, left: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -30,6 +30,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   style: FontStyles.boldTextStyle(),
                 ),
                 Container(
+                  margin: EdgeInsets.only(right: 20),
                   padding: EdgeInsets.all(3),
                   decoration: BoxDecoration(
                       color: appcolor.secondaryColor,
@@ -60,41 +61,146 @@ class _HomeScreenState extends State<HomeScreen> {
               height: 20.0,
             ),
             SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
               child: Row(
                 children: [
-                  Material(
-                    elevation: 5.0,
-                    borderRadius: BorderRadius.circular(20),
-                    child: Container(
-                      padding: EdgeInsets.all(14),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Image.asset(
-                            "assets/GreekSalad.png",
-                            height: 150,
-                            width: 150,
-                            fit: BoxFit.cover,
-                          ),
-                          Text(
-                            "Greek Salad",
-                            style: FontStyles.SemiBoldTextStyle(),
-                          ),
-                          SizedBox(height: 4.8,),
-                          Text(
-                            "A tasty Greek Salad",
-                            style: FontStyles.lightTextStyle(),
-                          ),
-                           SizedBox(height: 4.8,),
-                           Text(
-                            "\$25",
-                            style: FontStyles.SemiBoldTextStyle(),
-                          ),
-                        ],
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    child: Material(
+                      elevation: 5.0,
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        padding: EdgeInsets.all(14),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Image.asset(
+                              "assets/GreekSalad.png",
+                              height: 150,
+                              width: 150,
+                              fit: BoxFit.cover,
+                            ),
+                            Text(
+                              "Greek Salad",
+                              style: FontStyles.SemiBoldTextStyle(),
+                            ),
+                            SizedBox(
+                              height: 4.8,
+                            ),
+                            Text(
+                              "A tasty Greek Salad",
+                              style: FontStyles.lightTextStyle(),
+                            ),
+                            SizedBox(
+                              height: 4.8,
+                            ),
+                            Text(
+                              "\$25",
+                              style: FontStyles.SemiBoldTextStyle(),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15.0,
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    child: Material(
+                      elevation: 5.0,
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        padding: EdgeInsets.all(14),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Image.asset(
+                              "assets/GreekSalad.png",
+                              height: 150,
+                              width: 150,
+                              fit: BoxFit.cover,
+                            ),
+                            Text(
+                              "Ceaser's Salad",
+                              style: FontStyles.SemiBoldTextStyle(),
+                            ),
+                            SizedBox(
+                              height: 4.8,
+                            ),
+                            Text(
+                              "A tasty Ceaser Salad",
+                              style: FontStyles.lightTextStyle(),
+                            ),
+                            SizedBox(
+                              height: 4.8,
+                            ),
+                            Text(
+                              "\$35",
+                              style: FontStyles.SemiBoldTextStyle(),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   )
                 ],
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Container(
+              margin: EdgeInsets.only(right: 20.0),
+              child: Material(
+                elevation: 5.0,
+                borderRadius: BorderRadius.circular(20.0),
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        "assets/GreekSalad.png",
+                        height: 120,
+                        width: 120,
+                        fit: BoxFit.cover,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                              width: MediaQuery.of(context).size.width / 2,
+                              child: Text(
+                                "Roasted Chicken Salad",
+                                style: FontStyles.SemiBoldTextStyle(),
+                              )),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          Container(
+                              width: MediaQuery.of(context).size.width / 2,
+                              child: Text(
+                                "Chicken and Veggies",
+                                style: FontStyles.lightTextStyle(),
+                              )),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          Container(
+                              width: MediaQuery.of(context).size.width / 2,
+                              child: Text(
+                                "\$28",
+                                style: FontStyles.SemiBoldTextStyle(),
+                              )),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
               ),
             )
           ],
