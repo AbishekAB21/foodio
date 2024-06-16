6ed0aa3c63f4ca64f1efc003f7316b940e05ab13
AbishekAB21 <abishekab1805@outlook.com>
2024-06-17 00:11:58 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:BottomNavBar(),
+      home:SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
