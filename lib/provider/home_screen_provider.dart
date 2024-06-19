465d170a0155211e3fc94863221e5eaa0df8010d
AbishekAB21 <abishekab1805@outlook.com>
2024-06-19 12:52:17 +0530

    commit

--- lib/provider/home_screen_provider.dart
+++ lib/provider/home_screen_provider.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:foodio/services/database.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeScreenProvider extends ChangeNotifier {
   Stream? _foodItemsStream;
