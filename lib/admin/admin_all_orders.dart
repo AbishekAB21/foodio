90d87682ab5e858a955fbdb90ded0d2ae7760852
AbishekAB21 <abishekab1805@outlook.com>
2024-07-01 14:26:15 +0530

    commit

--- lib/admin/admin_all_orders.dart
+++ lib/admin/admin_all_orders.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:foodio/admin/provider/all_orders_provider.dart';
+import 'package:foodio/admin/widgets/finance_sort_buttons.dart';
 import 'package:foodio/utils/app_colors.dart';
 import 'package:foodio/utils/font_styles.dart';
+import 'package:foodio/widgets/reusable_snackbar.dart';
 import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
@@ -29,74 +31,132 @@ class AllOrders extends StatelessWidget {
                     height: 100, width: 100),
               );
             }
-
-            return ListView.builder(
-              itemCount: allOrdersProvider.orders.length,
-              itemBuilder: (context, index) {
-                /* 
-                "order" extracts a single order from the orders[] list coming 
-                from the AllOrdersProvider.
-
-                "Items" coverts this single data to list<dynamic> and makes it 
-                possible for the UI to display it
-                */
-                var order = allOrdersProvider.orders[index];
-                var items = order['items'] as List<dynamic>;
-
-                return Card(
-                  elevation: 2,
-                  color: appcolor.primaryColor,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5)),
-                  margin: EdgeInsets.all(10),
-                  child: Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        /* 
-                      Iterating over the list and for each "item" in "items" list
-                      a widget is created and displayed with the data provided.
-                      */
-                        ...items.map((item) {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 5),
-                            child: Text(
-                              "${item['Quantity']} x ${item['Name']}",
-                              style: FontStyles.SemiBoldTextStyle(),
-                            ),
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          allOrdersProvider.applyFilter(
+                            "All Time",
                           );
-                        }).toList(),
-                        SizedBox(height: 10),
-                        Text(
-                          "Ordered on: ${DateFormat('yyyy-MM-dd').format(order['orderDate'].toDate())}",
-                          style: FontStyles.SmallTextFont(),
+
+                          ReusableSnackBar().showSnackbar(context,
+                              "Sorted by All Time", appcolor.secondaryColor);
+                        },
+                        child: SortButton(
+                          title: "All Time",
                         ),
-                        SizedBox(height: 10),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Total: \$${order['total']}",
-                              style: FontStyles.MediumTextFontWithColor(),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          allOrdersProvider.applyFilter("This Year");
+
+                          ReusableSnackBar().showSnackbar(context,
+                              "Sorted by This Year", appcolor.secondaryColor);
+                        },
+                        child: SortButton(
+                          title: "This Year",
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          allOrdersProvider.applyFilter("This Month");
+
+                          ReusableSnackBar().showSnackbar(context,
+                              "Sorted by This Month", appcolor.secondaryColor);
+                        },
+                        child: SortButton(
+                          title: "This Month",
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          allOrdersProvider.applyFilter("Today");
+
+                          ReusableSnackBar().showSnackbar(context,
+                              "Sorted by Today", appcolor.secondaryColor);
+                        },
+                        child: SortButton(
+                          title: "Today",
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: allOrdersProvider.filteredOrders.length,
+                      itemBuilder: (context, index) {
+                        var order = allOrdersProvider.filteredOrders[index];
+                        var items = order['items'] as List<dynamic>;
+
+                        return Card(
+                          elevation: 2,
+                          color: appcolor.primaryColor,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5)),
+                          margin: EdgeInsets.all(10),
+                          child: Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ...items.map((item) {
+                                  return Padding(
+                                    padding:
+                                        const EdgeInsets.symmetric(vertical: 5),
+                                    child: Text(
+                                      "${item['Quantity']} x ${item['Name']}",
+                                      style: FontStyles.SemiBoldTextStyle(),
+                                    ),
+                                  );
+                                }).toList(),
+                                SizedBox(height: 10),
+                                Text(
+                                  "Ordered on: ${DateFormat('yyyy-MM-dd').format(order['orderDate'].toDate())}",
+                                  style: FontStyles.SmallTextFont(),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  "Ordered By: ${order["userName"]}, ${order["userCity"]}",
+                                  style: FontStyles.SmallTextFont(),
+                                ),
+                                SizedBox(height: 10),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Total: \$${order['total']}",
+                                      style:
+                                          FontStyles.MediumTextFontWithColor(),
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
-                            // Container(
-                            //   padding: EdgeInsets.all(5),
-                            //   decoration: BoxDecoration(
-                            //       color: appcolor.WariningColor,
-                            //       borderRadius: BorderRadius.circular(5)),
-                            //   child: Text(
-                            //     "Ordered",
-                            //     style: FontStyles.ReallySmallTextFontWhite(),
-                            //   ),
-                            // )
-                          ],
-                        ),
-                      ],
+                          ),
+                        );
+                      },
                     ),
                   ),
-                );
-              },
+                ],
+              ),
             );
           },
         ),
--- lib/admin/provider/all_orders_provider.dart
+++ lib/admin/provider/all_orders_provider.dart
@@ -3,29 +3,47 @@ import 'package:flutter/material.dart';
 
 class AllOrdersProvider with ChangeNotifier {
   List<Map<String, dynamic>> orders = [];
+  List<Map<String, dynamic>> filteredOrders = [];
+  String currentFilter = "All Time";
 
   AllOrdersProvider() {
     fetchOrders();
   }
+  
 
   Future<void> fetchOrders() async {
     try {
-   
-   /* 
-   Get all the data stored in "AllOrders" collection and convert it to a list 
-   and store it in "orders[]".
-
-   Then access this "orders[]" list in the Admin_All_orders screen to display
-   the data.
-   */
       QuerySnapshot snapshot =
           await FirebaseFirestore.instance.collection("AllOrders").get();
       orders = snapshot.docs
           .map((doc) => doc.data() as Map<String, dynamic>)
           .toList();
+      applyFilter(currentFilter); // Apply the initial filter
       notifyListeners();
     } catch (e) {
       print("Error fetching orders: $e");
     }
+  }
+
+  void applyFilter(String filter) {
+    currentFilter = filter;
+    DateTime now = DateTime.now();
+    filteredOrders = orders.where((order) {
+      DateTime orderDate = (order['orderDate'] as Timestamp).toDate();
+      if (filter == "Today") {
+        return orderDate.year == now.year &&
+            orderDate.month == now.month &&
+            orderDate.day == now.day;
+      } else if (filter == "This Month") {
+        return orderDate.year == now.year && orderDate.month == now.month;
+      } else if (filter == "This Year") {
+        return orderDate.year == now.year;
+      } else {
+        return true; // All Time
+      }
+    }).toList();
+    notifyListeners();
+
+    
   }
 }
